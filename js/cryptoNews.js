5826bd7eb21f5a23ddd38db4930e8aa5c8ed4e7c
“AnaIitico” <jtollinchi1971@gmail.com>
2021-05-14 15:51:11 -0400

    update fetch

--- js/cryptoNews.js
+++ js/cryptoNews.js
@@ -1,16 +1,16 @@
-// let's write some fumctions below //
 
-var symbol = ""
-var urlSymbol = " "
-const form = document.querySelector('.form-group');// needs to be coordinated with Andrena
-const input = document.getElementById('input-symbol');// needs to be coordinated with Andrena
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    symbol = input.value;
-    symbol = symbol.toUpperCase();
+
+var symbol = " ";
+var urlSymbol = " ";
+var symbolArray = [];
+const dropMenu = document.querySelector('.dropdown-item');
+const input = document.getElementById('symbol');
+// const input = document.getElementById('symbol2');
+// const input = document.getElementById('symbol3');
+dropMenu.addEventListener("click", e => {
+    symbol = input.innerText;
     urlSymbol = `https://cryptonews-api.com/api/v1?tickers=${symbol}&items=50&token=${APIKEY}`;
-    // console.log(`This is symbol in Submit: ${symbol}`);
-    // console.log(`This is URL in Submit: ${url}`);
+    console.log(`This is symbol in Submit: ${symbol}`);
     displaySymbol();
 })
 
@@ -18,31 +18,37 @@ function displaySymbol(){
     fetch(urlSymbol)
     .then(apiData1 => apiData1.json())
     .then(symbolData => {
-        // console.log(symbolData);
+        console.log(symbolData);
         // console.log(`This is symbol in Fetch: ${symbol}`);
         // console.log(`This is description: ${description}`);
         // console.log(`This is icon: ${icon}`);
         // console.log(degF)
-        let div = document.getElementById('symbol');
-        div.innerText = `${symbol.title}`;
-        div.innerText = `${symbol.text}`;
-        div.innerText = `${symbol.source_name}`;
-        div.innerText = `${symbol.date}`;
-        div.innerText = `${symbol.sentiment}`;
-        div.innerText = `${symbol.type}`;
-        div.innerText = `${symbol.news_url}`;
-        // console.log(symbol)
-        let span = document.getElementById('conditions');
-        span.innerText = `${degF.toString()}F ${description}`;
-        //Create icon image
-        var image = document.getElementById('image');
-        image.setAttribute('src', symbolData.image_url);
-        image.width = "250";
-        image.height = "250";
-        
-        let imageBin = document.getElementById('pic');
-        imageBin.append(image);
 
-        input.value = "";//for clearing the input
+        symbolArray = symbolData;
+      
+        console.log(`This is symbolArray`);
+        console.log(symbolArray);
+        // let div = document.getElementById('symbol');
+
+        // div.innerText = `${symbol.title}`;
+        // div.innerText = `${symbol.text}`;
+        // div.innerText = `${symbol.source_name}`;
+        // div.innerText = `${symbol.date}`;
+        // div.innerText = `${symbol.sentiment}`;
+        // div.innerText = `${symbol.type}`;
+        // div.innerText = `${symbol.news_url}`;
+        // // console.log(symbol)
+        // let span = document.getElementById('conditions');
+        // span.innerText = `${degF.toString()}F ${description}`;
+        // //Create icon image
+        // var image = document.getElementById('image');
+        // image.setAttribute('src', symbolData.image_url);
+        // image.width = "250";
+        // image.height = "250";
+        
+        // let imageBin = document.getElementById('pic');
+        // imageBin.append(image);
+
+        // input.value = "";//for clearing the input
     })
 }
