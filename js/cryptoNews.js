bdd10eca090237784b6b8d6bb258ad9707db16f2
“AnaIitico” <jtollinchi1971@gmail.com>
2021-05-15 14:16:45 -0400

    added breaking news fetch

--- js/cryptoNews.js
+++ js/cryptoNews.js
@@ -3,11 +3,63 @@
 var symbol = " ";
 var urlSymbol = " ";
 var symbolArray = [];
+var topRankingNews = [];
+var breakingNewsArray = [];
 var sentimentAnalysis = [];
-const dropMenu = document.querySelector('.dropdown-menu');
-const sentimentMenu = document.querySelector('.sentiment-menu');
+var breakingNewsURL = `https://cryptonews-api.com/api/v1/category?section=general&items=50&token=${APIKEY}`;
 
-dropMenu.addEventListener("click", e => {
+// --breakingNews EventListener Fetch and display
+// const breakingNews = document.getElementById('breakingNews');
+// const sentimentMenu = document.querySelector('.sentiment-menu');
+// breakingNews.addEventListener("click", e => {
+//     urlSymbol = `https://cryptonews-api.com/api/v1/category?section=general&items=50&token=${APIKEY}`;
+//     console.log(`This is symbol in Event Listener: ${symbol}`);
+//     displayBreakingNews();
+// })
+
+function displayBreakingNews(){
+    fetch(breakingNewsURL)
+    .then(apiData2 => apiData2.json())
+    .then(breakingNewsData => {
+        // console.log(breakingNewsData);
+
+        breakingNewsArray = breakingNewsData;
+      
+        console.log(`This is breakingNewsArray`);
+        console.log(breakingNewsArray);
+
+        const breakingNews = document.getElementById('breakingNews');
+        // let div = document.getElementById('symbol');
+
+        // div.innerText = `${symbol.title}`;
+        // div.innerText = `${symbol.text}`;
+        // div.innerText = `${symbol.source_name}`;
+        // div.innerText = `${symbol.date}`;
+        // div.innerText = `${symbol.sentiment}`;
+        // div.innerText = `${symbol.type}`;
+        // div.innerText = `${symbol.news_url}`;
+        // // console.log(symbol)
+        // let span = document.getElementById('conditions');
+        // span.innerText = `${degF.toString()}F ${description}`;
+        // //Create icon image
+        // var image = document.getElementById('image');
+        // image.setAttribute('src', symbolData.image_url);
+        // image.width = "250";
+        // image.height = "250";
+        
+        // let imageBin = document.getElementById('pic');
+        // imageBin.append(image);
+
+        // input.value = "";//for clearing the input
+    })
+}
+// -- END breakingNews EventListener Fetch and display
+
+
+// --newsMenu EventListener Fetch and display
+const newsMenu = document.querySelector('.dropdown-menu');
+const sentimentMenu = document.querySelector('.sentiment-menu');
+newsMenu.addEventListener("click", e => {
     symbol = e.target.innerText;
     urlSymbol = `https://cryptonews-api.com/api/v1?tickers=${symbol}&items=50&token=${APIKEY}`;
     console.log(`This is symbol in Event Listener: ${symbol}`);
@@ -52,3 +104,6 @@ function displaySymbol(){
         // input.value = "";//for clearing the input
     })
 }
+// -- END newsMenu EventListener Fetch and display
+
+displayBreakingNews()
